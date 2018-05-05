03a51ba39d86e3e6520ec4862349c09010f359ce
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-05-05 23:51:42 +0800

    模板方法模式

--- ReadingNotes/DaHuaSheJiMoShi/src/main/java/com/yao/chapter10_TemplateMethod/AbstractClass.java
+++ ReadingNotes/DaHuaSheJiMoShi/src/main/java/com/yao/chapter10_TemplateMethod/AbstractClass.java
@@ -4,6 +4,9 @@ package com.yao.chapter10_TemplateMethod;
  * Created by shanyao on 2018/5/5.
  */
 public abstract class AbstractClass {
+    public void baseMethod() {
+        System.out.println("公用不需要重写的方法");
+    }
     public abstract void PrimitiveOperation1();
     public abstract void PrimitiveOperation2();
 
