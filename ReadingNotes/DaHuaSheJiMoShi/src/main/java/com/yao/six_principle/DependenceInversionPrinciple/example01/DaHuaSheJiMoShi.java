30c115dd9d152293e84b4983c924cb53b014a00e
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-05-08 22:04:39 +0800

    读书笔记

---
+++ ReadingNotes/DaHuaSheJiMoShi/src/main/java/com/yao/six_principle/DependenceInversionPrinciple/example01/DaHuaSheJiMoShi.java
@@ -0,0 +1,10 @@
+package com.yao.six_principle.DependenceInversionPrinciple.example01;
+
+/**
+ * Created by shanyao on 2018/5/8.
+ */
+public class DaHuaSheJiMoShi {
+    public void read() {
+        System.out.println("阅读大化设计模式");
+    }
+}
---
+++ ReadingNotes/DaHuaSheJiMoShi/src/main/java/com/yao/six_principle/DependenceInversionPrinciple/example01/XiaoMing.java
@@ -0,0 +1,10 @@
+package com.yao.six_principle.DependenceInversionPrinciple.example01;
+
+/**
+ * Created by shanyao on 2018/5/8.
+ */
+public class XiaoMing {
+    public void read(DaHuaSheJiMoShi daHuaSheJiMoShi) {
+        daHuaSheJiMoShi.read();
+    }
+}
