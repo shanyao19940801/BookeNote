d51e05de93d0228a9a0dda22fdecc3cb94991746
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-06-20 22:18:22 +0800

    剑指offer

--- ReadingNotes/JianZhiOffer/src/jianzhi/chapter03/c03/Problem12.java
+++ ReadingNotes/JianZhiOffer/src/jianzhi/chapter03/c03/Problem12.java
@@ -2,8 +2,15 @@ package jianzhi.chapter03.c03;
 
 /**
  * Created by shanyao on 2018/6/19.
+ * 输入n，打印1到n的最大位数，
+ * 注意n没有限制，所以避免数字过大需要使用字符的形式打印
  */
 public class Problem12 {
+
+    /**
+     * 解法一，利用字符模拟数字加
+     * @param n
+     */
     public static void print(int n){
         char [] num = new char[n];
         while (!(increment(num))) {
@@ -33,7 +40,56 @@ public class Problem12 {
         return isOK;
 
     }
-    public static void main(String[] args) {
-        print(3);
+    ///*******方法一
+    ///*******方法二
+
+    /**
+     * 方法二 利用数字全排列打印
+     * @param n
+     */
+    public static void print2(int n) {
+        if (n <= 0)
+            return;
+        char[] num = new char[n];
+
+        for (int i = 0; i < 10; i++) {
+            num[0] = (char) (i + '0');
+            printToMax(num, n, 0);
+        }
+
     }
+
+    public static void printToMax(char[] num, int n,int index) {
+        if (index == n-1) {
+            printNum(num);
+            return;
+        }
+
+        for (int i = 0; i < 10;i++) {
+            num[index+1] = (char) (i + '0');
+            printToMax(num, n, index+1);
+        }
+    }
+
+    private static void printNum(char[] num) {
+        boolean begin = false;
+        for (int i = 0; i < num.length; i++) {
+            if (!begin && num[i] != '0') {
+                begin = true;
+            }
+            if (begin)
+                System.out.print(num[i]);
+        }
+        if (begin)
+            System.out.println();
+//        System.out.println(num);
+    }
+
+
+    public static void main(String[] args) {
+//        print(3);
+        print2(2);
+    }
+
+
 }
