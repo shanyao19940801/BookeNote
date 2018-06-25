6ac8dc3065484605279d1eb41c13d4886da2071e
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-06-25 22:48:16 +0800

    二叉树中和为n的某一路劲

---
+++ ReadingNotes/JianZhiOffer/src/jianzhi/chapter04/c03/Problem25.java
@@ -0,0 +1,71 @@
+package jianzhi.chapter04.c03;
+
+import jianzhi.Standard.TreeNode;
+import jianzhi.Util;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Created by shanyao on 2018/6/25.
+ */
+public class Problem25 {
+    //方法二
+    ArrayList<ArrayList<Integer>> lists = new ArrayList<ArrayList<Integer>>();
+    ArrayList<Integer> lu = new ArrayList<>();
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root,int target) {
+
+        if(root == null)
+            return lists;
+        target -= root.val;
+        lu.add(root.val);
+        if(root.left == null && root.right == null) {
+            if(target == 0)
+                lists.add(new ArrayList(lu));
+        }
+        if(root.left !=null) {
+            FindPath(root.left,target);
+        }
+        if(root.right != null) {
+            FindPath(root.right,target);
+        }
+        lu.remove(lu.size()-1);
+        target+=root.val;
+        return lists;
+    }
+
+    //方法一
+    /*public ArrayList<ArrayList<Integer>> FindPath(TreeNode root,int target) {
+        if(root == null)
+            return new ArrayList();
+        ArrayList<ArrayList<Integer>> lists = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> lu = new ArrayList<>();
+        lu.add(root.val);
+        getAllList(root,target,root.val,lu,lists);
+        return lists;
+    }
+
+    public void getAllList(TreeNode root,int target,int sum,ArrayList<Integer> lu,
+                           ArrayList<ArrayList<Integer>> lists) {
+        if(root.left == null && root.right == null) {
+            if(sum == target)
+                lists.add(lu);
+            return;
+        }
+
+        if(root.left != null) {
+            ArrayList<Integer> l = new ArrayList(lu);
+            l.add(root.left.val);
+            getAllList(root.left,target,sum+root.left.val,l,lists);
+        }
+        if(root.right != null) {
+            ArrayList<Integer> l = new ArrayList(lu);
+            l.add(root.right.val);
+            getAllList(root.right,target,sum+root.right.val,l,lists);
+        }
+    }*/
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+//        list.remove()
+    }
+}
