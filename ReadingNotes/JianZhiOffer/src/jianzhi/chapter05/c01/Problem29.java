5ac8026c34b53ce7481413976481b5da4c903a9b
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-06-27 22:45:54 +0800

    数组中出现次数超过一半的数字

--- ReadingNotes/JianZhiOffer/src/jianzhi/chapter05/c01/Problem29.java
+++ ReadingNotes/JianZhiOffer/src/jianzhi/chapter05/c01/Problem29.java
@@ -11,16 +11,23 @@ public class Problem29 {
             return 0;
         HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
         for(int i =0; i < array.length; i++) {
-            if(map.containsKey(i)) {
-                int temp = map.get(i);
-                map.put(i,temp+1);
-                if(map.get(i) > array.length/2) {
-                    return i;
+            if(map.containsKey(array[i])) {
+                int temp = map.get(array[i]);
+                map.put(array[i],temp+1);
+                if(map.get(array[i]) > array.length/2) {
+                    return array[i];
                 }
             } else {
-                map.put(i,0);
+                map.put(array[i],0);
             }
         }
         return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem29 p = new Problem29();
+        int [] a = {1,2,3,2,2,2,5,4,2};
+        int r = p.MoreThanHalfNum_Solution(a);
+        System.out.println(r);
     }
 }
---
+++ ReadingNotes/JianZhiOffer/src/jianzhi/niuke/MoreThanHalfNum_Solution.java
@@ -0,0 +1,12 @@
+package jianzhi.niuke;
+
+import jianzhi.chapter05.c01.Problem29;
+
+/**
+ * Created by shanyao on 2018/6/27.
+ */
+public class MoreThanHalfNum_Solution {
+    public static void main(String[] args) {
+        Problem29 p = new Problem29();
+    }
+}
