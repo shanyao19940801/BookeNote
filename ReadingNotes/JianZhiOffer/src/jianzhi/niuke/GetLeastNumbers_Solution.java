1bc9e96ea761af050397d8fad5c0891757a2b5df
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-07-01 23:18:48 +0800

    最小k个数

--- ReadingNotes/JianZhiOffer/src/jianzhi/niuke/GetLeastNumbers_Solution.java
+++ ReadingNotes/JianZhiOffer/src/jianzhi/niuke/GetLeastNumbers_Solution.java
@@ -1,7 +1,12 @@
 package jianzhi.niuke;
 
+import jianzhi.chapter05.c02.Problem30;
+
 /**
  * Created by shanyao on 2018/7/1.
  */
 public class GetLeastNumbers_Solution {
+    public static void main(String[] args) {
+        Problem30 p = new Problem30();
+    }
 }
