a8d47df1f0131afc784b87c5deae4862bcd7282f
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-07-03 22:32:14 +0800

    丑數

--- ReadingNotes/JianZhiOffer/src/jianzhi/chapter05/c03/Problem34.java
+++ ReadingNotes/JianZhiOffer/src/jianzhi/chapter05/c03/Problem34.java
@@ -5,6 +5,38 @@ package jianzhi.chapter05.c03;
  */
 public class Problem34 {
     public int GetUglyNumber_Solution(int index) {
+        int [] ugly = new int[index];
+        ugly[0] = 1;
+        int t2 = 0;
+        int t3 = 0;
+        int t5 = 0;
+        int ugnum = 1;
+        while (ugnum < index) {
+            int min = min(ugly[t2]*2,ugly[t3]*3,ugly[t5]*5);
+            if (min > ugly[ugnum - 1]) {
+                ugly[ugnum] = min;
+            }
+            while (ugly[t2]*2 <= ugly[ugnum]) {
+                t2++;
+            }
+            while (ugly[t3]*3 <= ugly[ugnum]) {
+                t3++;
+            }
+            while (ugly[t5]*5 <= ugly[ugnum]) {
+                t5++;
+            }
+            ugnum++;
+        }
+        return ugly[ugnum-1];
+
+    }
+
+    public int min(int t1,int t3,int t5){
+        int min = t1 < t3 ? t1 : t3;
+        return min < t5 ? min :t5;
+
+    }
+    public int GetUglyNumber_Solution1(int index) {
         int num = 0;
         int count = 0;
         while(count< index) {
@@ -32,6 +64,6 @@ public class Problem34 {
 
     public static void main(String[] args) {
         Problem34 p = new Problem34();
-        System.out.println(p.GetUglyNumber_Solution(10));
+        System.out.println(p.GetUglyNumber_Solution(1500));
     }
 }
