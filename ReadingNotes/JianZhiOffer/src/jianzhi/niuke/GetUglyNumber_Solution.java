f7a29541a46104086f6b98963b8f0192d37a33c3
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-07-03 22:32:45 +0800

    丑數

---
+++ ReadingNotes/JianZhiOffer/src/jianzhi/niuke/GetUglyNumber_Solution.java
@@ -0,0 +1,12 @@
+package jianzhi.niuke;
+
+import jianzhi.chapter05.c03.Problem34;
+
+/**
+ * Created by shanyao on 2018/7/3.
+ */
+public class GetUglyNumber_Solution {
+    public static void main(String[] args) {
+        Problem34 p = new Problem34();
+    }
+}
