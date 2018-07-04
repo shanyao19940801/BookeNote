9c9b218bb72d7b2880908aef03255c17e54ac651
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-07-04 22:51:08 +0800

    数字在排序数组中出现的次数

---
+++ ReadingNotes/JianZhiOffer/src/jianzhi/niuke/GetNumberOfK.java
@@ -0,0 +1,12 @@
+package jianzhi.niuke;
+
+import jianzhi.chapter06.Problem38;
+
+/**
+ * Created by shanyao on 2018/7/4.
+ */
+public class GetNumberOfK {
+    public static void main(String[] args) {
+        Problem38 p = new Problem38();
+    }
+}
