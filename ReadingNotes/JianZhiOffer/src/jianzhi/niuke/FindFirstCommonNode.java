24440ac5adc0f86a1c078de11407b3ef0c750b1f
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-07-04 22:26:45 +0800

    两个链表的第一个公共结点

---
+++ ReadingNotes/JianZhiOffer/src/jianzhi/niuke/FindFirstCommonNode.java
@@ -0,0 +1,11 @@
+package jianzhi.niuke;
+
+import jianzhi.chapter04.c03.Problem22;
+import jianzhi.chapter05.c03.Problem37;
+
+/**
+ * Created by shanyao on 2018/7/4.
+ */
+public class FindFirstCommonNode {
+    Problem37 p = new Problem37();
+}
