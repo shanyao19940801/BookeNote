58fadfdb423b5e5256b18ddbf16be72c4df880bd
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-07-04 22:06:48 +0800

    两个链表的第一个公共结点

--- ReadingNotes/JianZhiOffer/src/jianzhi/chapter05/c03/Problem37.java
+++ ReadingNotes/JianZhiOffer/src/jianzhi/chapter05/c03/Problem37.java
@@ -2,12 +2,35 @@ package jianzhi.chapter05.c03;
 
 import jianzhi.Standard.ListNode;
 
+import java.util.Stack;
+
 /**
  * Created by shanyao on 2018/7/4.
  */
 public class Problem37 {
-    //方法一：先算出两个链表第一个公共节点之前相差节点n个，然后把多个链表前移n个后两个链表一起前移知道相同为止
+    //方法二：利用两个栈
     public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+        if(pHead1 == null || pHead2 == null)
+            return null;
+        Stack<ListNode> s1 = new Stack<ListNode>();
+        Stack<ListNode> s2 = new Stack<ListNode>();
+        while(pHead1 != null) {
+            s1.push(pHead1);
+            pHead1 = pHead1.next;
+        }
+        while(pHead2 != null) {
+            s2.push(pHead2);
+            pHead2 = pHead2.next;
+        }
+        ListNode r = null;
+        while(!s1.isEmpty() && !s2.isEmpty() && (s1.peek()) == s2.peek()) {
+            s1.pop();
+            r = s2.pop();
+        }
+        return r;
+    }
+    //方法一：先算出两个链表第一个公共节点之前相差节点n个，然后把多个链表前移n个后两个链表一起前移知道相同为止
+    /*public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
         //方法一
         if(pHead1 == null || pHead2 == null)
             return null;
@@ -40,5 +63,7 @@ public class Problem37 {
             pHead2 = pHead2.next;
         }
         return pHead1;
-    }
+    }*/
+
+
 }
