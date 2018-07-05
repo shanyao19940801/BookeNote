a8ce061b1479ed5d658d7ad82f4ea9fa6545f2a4
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-07-05 22:55:49 +0800

    问题41

---
+++ ReadingNotes/JianZhiOffer/src/jianzhi/chapter06/c03/Problem42.java
@@ -0,0 +1,56 @@
+package jianzhi.chapter06.c03;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+/**
+ * Created by shanyao on 2018/7/5.
+ */
+public class Problem42 {
+    public ArrayList<ArrayList<Integer> > FindContinuousSequence(int sum) {
+        ArrayList<ArrayList<Integer>> lists = new ArrayList<ArrayList<Integer>>();
+        if(sum < 3)
+            return lists;
+        int end = sum/2 + 1;
+        int start = 1;
+        while (!isEnd(sum,start)) {
+            start++;
+        }
+        while(start <= end) {
+            int index = start;
+            ArrayList<Integer> list = new ArrayList();
+            int tempSum = 0;
+            while(tempSum < sum ) {
+                list.add(index);
+                tempSum += index;
+                index--;
+            }
+            if(tempSum == sum) {
+                Collections.sort(list,new MyComtrator());
+                lists.add(list);
+            }
+            start++;
+        }
+        return lists;
+    }
+
+
+
+    boolean isEnd(int sum,int index) {
+        if(index*(index+1) < 2*sum){
+            return false;
+        }
+        return true;
+    }
+
+    static class MyComtrator implements Comparator<Integer> {
+
+        @Override
+        public int compare(Integer o1, Integer o2) {
+
+            return o1 > o2 ? 1 : -1;
+        }
+    }
+
+}
---
+++ ReadingNotes/JianZhiOffer/src/jianzhi/niuke/FindNumbersWithSum.java
@@ -0,0 +1,10 @@
+package jianzhi.niuke;
+
+import jianzhi.chapter06.c03.Problem41;
+
+/**
+ * Created by shanyao on 2018/7/5.
+ */
+public class FindNumbersWithSum {
+    Problem41 p = new Problem41();
+}
