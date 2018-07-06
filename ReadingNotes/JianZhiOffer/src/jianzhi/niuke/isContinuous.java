38ffde1e92fb26fd7e6174ed8906ca8531a6f142
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-07-06 21:33:57 +0800

    扑克牌顺序

---
+++ ReadingNotes/JianZhiOffer/src/jianzhi/niuke/isContinuous.java
@@ -0,0 +1,10 @@
+package jianzhi.niuke;
+
+import jianzhi.chapter06.c04.Problem44;
+
+/**
+ * Created by shanyao on 2018/7/6.
+ */
+public class isContinuous {
+    Problem44 p = new Problem44();
+}
