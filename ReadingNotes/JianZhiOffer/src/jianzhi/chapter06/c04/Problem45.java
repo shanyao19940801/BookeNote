4d338e8c47ee392e59b344ff0d4f4cdbe9400e19
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-07-06 22:21:18 +0800

    孩子们的游戏(圆圈中最后剩下的数)

---
+++ ReadingNotes/JianZhiOffer/src/jianzhi/chapter06/c04/Problem45.java
@@ -0,0 +1,50 @@
+package jianzhi.chapter06.c04;
+
+import jianzhi.Standard.ListNode;
+
+/**
+ * Created by shanyao on 2018/7/6.
+ */
+public class Problem45 {
+    public int LastRemaining_Solution(int n, int m) {
+        if(n == 1) {
+            return 0;
+        }
+        int [] num = new int[n];
+        for(int i = 0; i < n; i++) {
+            num[i] = i;
+        }
+
+        int leftNum = n;
+        int in = 0;
+        while(leftNum>1) {
+            int count = 0;
+            in %= n;
+            while(count<m) {
+                if(num[in++%n]== -1) {
+                    continue;
+                }
+
+//                num[in++%n] = -1;
+                count++;
+            }
+
+            num[(in-1)%n] = -1;
+//            in++;
+//            num[in++%n] = -1;
+            leftNum--;
+        }
+        for(int i =0; i< n; i++) {
+            if(num[i] != -1)
+                return num[i];
+        }
+        return 0;
+
+    }
+
+    public static void main(String[] args) {
+        Problem45 p = new Problem45();
+        int l = p.LastRemaining_Solution(5,3);
+        System.out.println(l);
+    }
+}
