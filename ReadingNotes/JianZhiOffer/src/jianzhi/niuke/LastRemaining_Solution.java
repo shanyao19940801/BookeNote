ae2a6df0ff28d3192b6f67d75c4ebc44d2eb1c5e
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-07-06 22:21:59 +0800

    孩子们的游戏(圆圈中最后剩下的数)

---
+++ ReadingNotes/JianZhiOffer/src/jianzhi/niuke/LastRemaining_Solution.java
@@ -0,0 +1,10 @@
+package jianzhi.niuke;
+
+import jianzhi.chapter06.c04.Problem45;
+
+/**
+ * Created by shanyao on 2018/7/6.
+ */
+public class LastRemaining_Solution {
+    Problem45 p = new Problem45();
+}
