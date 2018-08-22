ca635a8aca2feccf1dff9838626f0ada8b7575a9
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-08-22 23:10:15 +0800

    深入理解Java虚拟机

---
+++ JVM/codeshow/src/com/jvm/chapter02/HeapOOM.java
@@ -0,0 +1,17 @@
+package com.jvm.chapter02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HeapOOM {
+    static class OOMObject {
+
+    }
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<>();
+
+        while (true) {
+            list.add(new OOMObject());
+        }
+    }
+}
