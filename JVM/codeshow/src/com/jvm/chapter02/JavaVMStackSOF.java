e3f79836a0fcb70970db8adc48176d8b66d8ef3f
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-08-23 22:14:17 +0800

    内存溢出

---
+++ JVM/codeshow/src/com/jvm/chapter02/JavaVMStackSOF.java
@@ -0,0 +1,22 @@
+package com.jvm.chapter02;
+
+public class JavaVMStackSOF {
+    private int stackLength = 1;
+
+    public void stackLeak() {
+        stackLength++;
+        stackLeak();
+    }
+
+    public static void main(String[] args) {
+        JavaVMStackSOF oom = new JavaVMStackSOF();
+        try {
+            oom.stackLeak();
+        } catch (Throwable e) {
+            System.out.println("stack length:" + oom.stackLength);
+//            e.printStackTrace();
+            throw e;
+
+        }
+    }
+}
