ca31d07362a9ceafc15f3005e29ffb47feadc55b
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-08-28 21:55:40 +0800

    1

--- JVM/codeshow/src/com/jvm/chapter03/ReferenceCountingGC.java
+++ JVM/codeshow/src/com/jvm/chapter03/ReferenceCountingGC.java
@@ -1,5 +1,10 @@
 package com.jvm.chapter03;
 
+/**
+ * 此代码演示了两点：
+ * 1.对象可以在被GC时自我拯救。
+ * 2.这种自救的机会只有一次，因为对象的finalize()方法最多只会被系统自动调用一次
+ */
 public class ReferenceCountingGC {
     public Object instance = null;
     private static final int _1MB = 1024 * 1024;
