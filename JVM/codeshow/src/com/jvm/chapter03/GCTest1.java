5a63b2b36f7dee8486171b9c58cab4648b8dd7bf
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-09-03 23:17:27 +0800

    垃圾收集器

--- JVM/codeshow/src/com/jvm/chapter03/GCTest1.java
+++ JVM/codeshow/src/com/jvm/chapter03/GCTest1.java
@@ -12,9 +12,8 @@ public class GCTest1 {
         allocation1 = new byte[2 * _1MB];
         allocation2 = new byte[2 * _1MB];
         allocation3 = new byte[2 * _1MB];
-        System.gc();
-        allocation4 = new byte[134 * _1MB];//出现一次Minor GC
-        System.gc();
+        allocation4 = new byte[4 * _1MB];//出现一次Minor GC
+//        System.gc();
     }
 
 
