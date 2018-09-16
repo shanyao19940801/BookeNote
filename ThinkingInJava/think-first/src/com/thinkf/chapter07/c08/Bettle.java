9a999d88e7dab959fb9049df8d9dbf800b0d0f1f
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-09-16 22:07:16 +0800

    类加载顺序

---
+++ ThinkingInJava/think-first/src/com/thinkf/chapter07/c08/Bettle.java
@@ -0,0 +1,38 @@
+package com.thinkf.chapter07.c08;
+
+class B {
+    public B() {
+        System.out.println("b");
+    }
+}
+class A {
+    static B b = new B();
+    B v1 = new B();
+    static {
+        System.out.println("static A");
+    }
+    public A() {
+        System.out.println("A");
+    }
+}
+class C {
+    public C() {
+        System.out.println("C");
+    }
+}
+public class Bettle extends A {
+    static C c = new C();
+
+    static {
+        System.out.println("static Bettle");
+    }
+    public Bettle() {
+        System.out.println("Bettle");
+    }
+
+    public static void main(String[] args) {
+        Bettle b = new Bettle();
+        System.out.println("------");
+        Bettle b1 = new Bettle();
+    }
+}
