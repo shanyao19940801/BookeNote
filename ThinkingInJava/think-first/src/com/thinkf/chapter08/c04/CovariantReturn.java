6085a3be3ebeeca00b8b46062e10ca356cf6c915
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-09-16 23:36:30 +0800

    no message

--- ThinkingInJava/think-first/src/com/thinkf/chapter08/c04/CovariantReturn.java
+++ ThinkingInJava/think-first/src/com/thinkf/chapter08/c04/CovariantReturn.java
@@ -1,4 +1,35 @@
 package com.thinkf.chapter08.c04;
 
+class Grain{
+    @Override
+    public String toString() {
+        return "Grain{}";
+    }
+}
+class Wheat extends Grain {
+    @Override
+    public String toString() {
+        return "Wheat{}";
+    }
+
+}
+class Mill {
+    Grain process() {
+        return new Grain();
+    }
+}
+class WheatMill extends Mill {
+    Wheat process() {
+        return new Wheat();
+    }
+}
 public class CovariantReturn {
+    public static void main(String[] args) {
+        Mill m = new Mill();
+        Grain g = m.process();
+        System.out.println(g);
+        m = new WheatMill();
+        g = m.process();
+        System.out.println(g);
+    }
 }
