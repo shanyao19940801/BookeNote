3ed712e83f67963302635daff8ead61e0d8f6023
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-09-20 21:36:10 +0800

    java编程思想

---
+++ ThinkingInJava/think-first/src/com/thinkf/chapter09/c04/Adventure.java
@@ -0,0 +1,20 @@
+package com.thinkf.chapter09.c04;
+
+interface CanFight{
+    void fight();
+}
+
+class ActionCharacter{
+    public void fight(){
+        System.out.println("ff");
+    }
+}
+
+class Hero extends ActionCharacter implements CanFight {
+    
+}
+public class Adventure {
+    public static void main(String[] args) {
+
+    }
+}
