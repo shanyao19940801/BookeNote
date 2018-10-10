2a2f1c780669306cb77800eefd2d7d72cd25ed3f
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-10-10 23:11:56 +0800

    Java中回调

---
+++ java/code/src/com/code/callback/CallBack.java
@@ -0,0 +1,14 @@
+package com.code.callback;
+
+/**
+ *  * 这是一个回调接口
+ */
+public interface CallBack {
+
+    /**
+     *      * 这个是小李知道答案时要调用的函数告诉小王，也就是回调函数
+     *      * @param result 是答案
+     *      
+     */
+    public void solve(String result);
+}
---
+++ java/code/src/com/code/callback/Test.java
@@ -0,0 +1,25 @@
+package com.code.callback;
+
+/**
+ * 测试类
+ *
+ * @author xiaanming
+ */
+public class Test {
+    public static void main(String[] args) {
+/**
+ * new 一个小李
+ */
+        Li li = new Li();
+
+/**
+ * new 一个小王
+ */
+        Wang wang = new Wang(li);
+
+/**
+ * 小王问小李问题
+ */
+        wang.askQuestion("1 + 1 = ?");
+    }
+}
