6f27ecf9b7d30395cbefab828eed26c5c6b1cc3c
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-10-18 22:11:17 +0800

    no message

---
+++ ThinkingInJava/think-first/src/com/thinkf/chapter10/c10/BigEgg.java
@@ -0,0 +1,26 @@
+package com.thinkf.chapter10.c10;
+
+class Egg {
+    private Yolk y;
+    protected class Yolk {
+        public Yolk() {
+            System.out.println("Egg.Yolk");
+        }
+    }
+    public Egg() {
+        System.out.println("New Egg()");
+        y = new Yolk();
+    }
+}
+
+public class BigEgg extends Egg{
+    public class Yolk {
+        public Yolk() {
+            System.out.println("BigEgg.Yolk()");
+        }
+
+    }
+    public static void main(String[] args) {
+        new BigEgg();
+    }
+}
