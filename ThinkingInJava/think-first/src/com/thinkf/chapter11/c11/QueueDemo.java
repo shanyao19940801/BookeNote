6025f83838fdca5a1df0630cd20c6f41a30f8e1a
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-10-23 23:15:08 +0800

    no message

--- ThinkingInJava/think-first/src/com/thinkf/chapter11/c11/QueueDemo.java
+++ ThinkingInJava/think-first/src/com/thinkf/chapter11/c11/QueueDemo.java
@@ -5,7 +5,7 @@ import java.util.Queue;
 public class QueueDemo {
     public static void printQ(Queue queue) {
         while (queue.peek() != null) {
-            System.out.println(queue.remove() + " ");
+            System.out.print(queue.remove() + " ");
         }
         System.out.println();
     }
