ccba4842bdf4aa5d7d6e08bdfa7ee2147c5cf880
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-10-23 23:10:34 +0800

    Java编程思想

---
+++ ThinkingInJava/think-first/src/com/thinkf/chapter11/c11/PriorityQueueDemo.java
@@ -0,0 +1,37 @@
+package com.thinkf.chapter11.c11;
+
+import java.util.*;
+
+public class PriorityQueueDemo {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        Random rand = new Random(47);
+        for (int i = 0; i < 10; i++) {
+            priorityQueue.offer(rand.nextInt(i + 10));
+        }
+        QueueDemo.printQ(priorityQueue);
+        List<Integer> ints = Arrays.asList(25, 22, 20, 18, 14, 9, 3, 1, 1,2, 3,18,21,23,25);
+        priorityQueue = new PriorityQueue<>(ints);
+        QueueDemo.printQ(priorityQueue);
+        priorityQueue = new PriorityQueue<>(
+                ints.size(), Collections.reverseOrder()
+        );
+
+        String fact = "EDUCATION SHOULD ESCHEW QBFUSCATION";
+        List<String> strings = Arrays.asList(fact.split(" "));
+        PriorityQueue<String> stringPQ = new PriorityQueue<>(
+                strings.size(), Collections.reverseOrder()
+        );
+        stringPQ.addAll(strings);
+        QueueDemo.printQ(stringPQ);
+
+        Set<Character> charSet = new HashSet<>();
+        for (char c : fact.toCharArray()) {
+            charSet.add(c);
+        }
+        PriorityQueue<Character> characterPQ =
+                new PriorityQueue<>(charSet);
+
+        QueueDemo.printQ(characterPQ);
+    }
+}
---
+++ ThinkingInJava/think-first/src/com/thinkf/chapter11/c11/QueueDemo.java
@@ -0,0 +1,12 @@
+package com.thinkf.chapter11.c11;
+
+import java.util.Queue;
+
+public class QueueDemo {
+    public static void printQ(Queue queue) {
+        while (queue.peek() != null) {
+            System.out.println(queue.remove() + " ");
+        }
+        System.out.println();
+    }
+}
