b24f5978644353fefe2b459dfdf34e5bda7904b5
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-11-04 21:59:06 +0800

    java编程思想

--- ThinkingInJava/think-first/src/com/thinkf/chapter11/c13/IterableClass.java
+++ ThinkingInJava/think-first/src/com/thinkf/chapter11/c13/IterableClass.java
@@ -3,8 +3,20 @@ package com.thinkf.chapter11.c13;
 import java.util.Iterator;
 
 public class IterableClass implements Iterable<String> {
+    protected  String[] worlds = ("I an Shan Yao").split(" ");
     @Override
     public Iterator<String> iterator() {
-        return null;
+        return new Iterator<String>() {
+            int index = 0;
+            @Override
+            public boolean hasNext() {
+                return index < worlds.length;
+            }
+
+            @Override
+            public String next() {
+                return worlds[index++];
+            }
+        };
     }
 }
