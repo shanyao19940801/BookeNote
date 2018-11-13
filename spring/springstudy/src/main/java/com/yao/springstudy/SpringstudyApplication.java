8bbb6a165e0cbff08ada25e2b9c728f35e545a5a
shanyaoself <sqshanyao@163.com>
2018-11-13 17:04:13 +0800

    springstudy

---
+++ spring/springstudy/src/main/java/com/yao/springstudy/SpringstudyApplication.java
@@ -0,0 +1,12 @@
+package com.yao.springstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringstudyApplication.class, args);
+    }
+}
