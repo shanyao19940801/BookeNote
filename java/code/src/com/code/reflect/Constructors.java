58b0ff67e1500db98052fd9461579958e805332f
shanyaoself <sqshanyao@163.com>
2018-11-13 11:52:18 +0800

    反射

--- java/code/src/com/code/reflect/Constructors.java
+++ java/code/src/com/code/reflect/Constructors.java
@@ -1,6 +1,21 @@
 package com.code.reflect;
 
 import java.lang.reflect.Constructor;
+/*
+ * 通过Class对象可以获取某个类中的：构造方法、成员变量、成员方法；并访问成员；
+ *
+ * 1.获取构造方法：
+ * 		1).批量的方法：
+ * 			public Constructor[] getConstructors()：所有"公有的"构造方法
+            public Constructor[] getDeclaredConstructors()：获取所有的构造方法(包括私有、受保护、默认、公有)
+
+ * 		2).获取单个的方法，并调用：
+ * 			public Constructor getConstructor(Class... parameterTypes):获取单个的"公有的"构造方法：
+ * 			public Constructor getDeclaredConstructor(Class... parameterTypes):获取"某个构造方法"可以是私有的，或受保护、默认、公有；
+ *
+ * 			调用构造方法：
+ * 			Constructor-->newInstance(Object... initargs)
+ */
 
 public class Constructors {
     public static void main(String[] args) {
--- java/code/src/com/code/reflect/Student.java
+++ java/code/src/com/code/reflect/Student.java
@@ -1,6 +1,21 @@
 package com.code.reflect;
 
 public class Student {
+    public String name;
+    protected int age;
+    char sex;
+    private String phoneNum;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                ", phoneNum='" + phoneNum + '\'' +
+                '}';
+    }
+
     //---------------构造方法-------------------
     //（默认的构造方法）
     Student(String str){
