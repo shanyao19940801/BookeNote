8feb59df073d944b13d4696efb6758d8c780af52
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-11-17 23:32:58 +0800

    no message

---
+++ SringInAction/springinaction/src/main/java/com/spring/springinaction/SpringinactionApplication.java
@@ -0,0 +1,12 @@
+package com.spring.springinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringinactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringinactionApplication.class, args);
+    }
+}
