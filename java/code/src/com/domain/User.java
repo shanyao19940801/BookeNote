5ac3a2e4c2313c121f7d9e3341e012c1e438fc6a
shanyaoself <sqshanyao@163.com>
2018-11-22 16:27:56 +0800

    java

--- java/code/src/com/domain/User.java
+++ java/code/src/com/domain/User.java
@@ -1,5 +1,5 @@
 package com.domain;
 
 public class User {
-    
+
 }
--- java/javaknowledge/src/main/java/skill/SortSkill.java
+++ java/javaknowledge/src/main/java/skill/SortSkill.java
@@ -5,15 +5,14 @@ import com.google.common.collect.ComparisonChain;
 import java.util.Comparator;
 
 public class SortSkill implements Comparator {
-    @Override
+    /*@Override
     public int compare(AnswerPackage o1, AnswerPackage o2) {
         return ComparisonChain.start()
                 .compare(o1.getContainTimeLength(), o2.getContainTimeLength())
                 .compare(o2.getDiscountPrice(), o1.getDiscountPrice())
                 .result();
-    }
+    }*/
 
-    @Override
     public int compare(Object o1, Object o2) {
         return 0;
     }
