927202fe715ac4261ecae1872c474927818884ac
shanyaoself <sqshanyao@163.com>
2018-11-23 18:06:30 +0800

    no message

--- java/javaknowledge/src/main/java/code/annotation/Name.java
+++ java/javaknowledge/src/main/java/code/annotation/Name.java
@@ -1,14 +1,30 @@
 package code.annotation;
 
 public enum Name {
-    yao(1, "shanyao"),
-    hao(2, "shanhao");
+    yao(1, "shan", "yao"),
+    hao(2, "shan", "hao");
 
     private Integer num;
+    private String lastName;
     private String name;
-
-    Name(Integer num, String name) {
+    Name(Integer num, String name, String lastName) {
         this.name = name;
         this.num = num;
+        this.lastName = lastName;
     }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    
+
 }
--- java/javaknowledge/src/main/java/code/annotation/TestMain.java
+++ java/javaknowledge/src/main/java/code/annotation/TestMain.java
@@ -3,6 +3,6 @@ package code.annotation;
 @MyAnnotation(value = Name.hao)
 public class TestMain {
     public static void main(String[] args) {
-
+        System.out.println(Name.hao.getNum());
     }
 }
