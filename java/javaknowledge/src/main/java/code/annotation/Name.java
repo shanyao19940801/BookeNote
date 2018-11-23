9529cce21c652118e2834212a02db2008e24086f
shanyaoself <sqshanyao@163.com>
2018-11-23 18:13:07 +0800

    no message

--- java/javaknowledge/src/main/java/code/annotation/Name.java
+++ java/javaknowledge/src/main/java/code/annotation/Name.java
@@ -25,6 +25,17 @@ public enum Name {
         return this.name;
     }
 
-    
+    public static Name valueOf(Integer num) throws Exception {
+        Name[] var1 = values();
+        int var2 = var1.length;
+
+        for (int var3 = 0; var3 < var2; ++var3) {
+            Name name = var1[var3];
+            if (name.getNum().equals(num)) {
+                return name;
+            }
+        }
+        throw new Exception("miss");
+    }
 
 }
--- java/javaknowledge/src/main/java/code/annotation/TestMain.java
+++ java/javaknowledge/src/main/java/code/annotation/TestMain.java
@@ -4,5 +4,11 @@ package code.annotation;
 public class TestMain {
     public static void main(String[] args) {
         System.out.println(Name.hao.getNum());
+        try {
+            System.out.println(Name.valueOf(1));
+            System.out.println(Name.valueOf("shan"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
