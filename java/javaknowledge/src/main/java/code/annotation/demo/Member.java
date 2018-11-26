238665788a9a97673aa6718444efb569cecf83bd
shanyaoself <sqshanyao@163.com>
2018-11-26 16:23:49 +0800

    no message

--- java/javaknowledge/src/main/java/code/annotation/demo/Member.java
+++ java/javaknowledge/src/main/java/code/annotation/demo/Member.java
@@ -6,6 +6,47 @@ package code.annotation.demo;
 @DBTable(name = "MEMBER")
 public class Member {
     //主键ID
-    @SQLString(name = "ID")
+    @SQLString(name = "ID", value = 50, constraint = @Constraints(primarKey = true))
     private String id;
+
+    @SQLString(name = "NAME", value = 30)
+    private String name;
+
+    @SQLInteger(name = "AGE")
+    private int age;
+
+    @SQLString(name = "DESCRIPTION", value = 150, constraint = @Constraints(allowNull = true))
+    private String description;//个人描述
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
