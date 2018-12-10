fe67298e0984d0c68e0639b444ff10ef1767a489
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-12-10 23:06:08 +0800

    no message

--- SringInAction/file/chapter4.md
+++ SringInAction/file/chapter4.md
@@ -101,3 +101,30 @@ Spring支持方法级别的连接点
 
 * 使用@Pointcut定义一个可重用的切点
 
+		@Aspect
+		public class Audience {
+		    //定义一个可重用的切点
+		    //该方法内容应该是一个空的，本身只是一个标识
+		    @Pointcut("execution(** chapter04.Performance.perform(..))")
+		    public void performance(){}
+		    //前置通知
+		    @Before("performance()")
+		    public void silenceCellPhone() {
+		        System.out.println("关闭手机");
+		    }
+		    //前置通知
+		    @Before("performance()")
+		    public void takeSeats() {
+		        System.out.println("坐好");
+		    }
+		    //后置通知
+		    @AfterReturning("performance()")
+		    public void applause() {
+		        System.out.println("鼓掌");
+		    }
+		    //异常通知
+		    @AfterThrowing("performance()")
+		    public void demandRefund() {
+		        System.out.println("失败了，没关系，重新开始！");
+		    }
+		}
--- SringInAction/springinaction/chapter02/src/main/java/chapter04/Audience.java
+++ SringInAction/springinaction/chapter02/src/main/java/chapter04/Audience.java
@@ -1,29 +1,31 @@
 package chapter04;
 
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
+import org.springframework.context.annotation.Bean;
 
 @Aspect
 public class Audience {
+    //定义一个可重用的切点
+    //该方法内容应该是一个空的，本身只是一个标识
+    @Pointcut("execution(** chapter04.Performance.perform(..))")
+    public void performance(){}
     //前置通知
-    @Before("execution(** chapter04.Performance.perform(..))")
+    @Before("performance()")
     public void silenceCellPhone() {
         System.out.println("关闭手机");
     }
     //前置通知
-    @Before("execution(** chapter04.Performance.perform(..))")
+    @Before("performance()")
     public void takeSeats() {
         System.out.println("坐好");
     }
     //后置通知
-    @AfterReturning("execution(** chapter04.Performance.perform(..))")
+    @AfterReturning("performance()")
     public void applause() {
         System.out.println("鼓掌");
     }
     //异常通知
-    @AfterThrowing("execution(** chapter04.Performance.perform(..))")
+    @AfterThrowing("performance()")
     public void demandRefund() {
         System.out.println("失败了，没关系，重新开始！");
     }
