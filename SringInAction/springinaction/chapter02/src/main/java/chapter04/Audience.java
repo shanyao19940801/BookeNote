2aaacd2a7d68fdf5adb0ec858b500deddc8dc815
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-12-10 22:48:31 +0800

    SPirng In Aciton

--- SringInAction/springinaction/chapter02/src/main/java/chapter04/Audience.java
+++ SringInAction/springinaction/chapter02/src/main/java/chapter04/Audience.java
@@ -1,7 +1,13 @@
 package chapter04;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class Audience {
+    //前置通知
+    @Before("execution(** chapter04.Performance.perform(..))")
+    public void silenceCellPhone() {
+        System.out.println("关闭手机");
+    }
 }
