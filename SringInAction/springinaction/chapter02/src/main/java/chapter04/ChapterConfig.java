85b2fd8649a3dd8bf138ce80ac8d36020093855a
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-12-10 23:07:09 +0800

    SPirng In Aciton

--- SringInAction/springinaction/chapter02/src/main/java/chapter04/ChapterConfig.java
+++ SringInAction/springinaction/chapter02/src/main/java/chapter04/ChapterConfig.java
@@ -1,4 +1,17 @@
 package chapter04;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy//启用AspectJ自动代理
+@ComponentScan
 public class ChapterConfig {
+
+    @Bean
+    public Audience audience() {
+        return new Audience();
+    }
 }
