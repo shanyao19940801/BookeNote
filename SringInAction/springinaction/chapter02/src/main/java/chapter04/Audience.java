4d26850007dcf48b26eabb8a5c4103c0f05213e7
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-12-10 22:53:28 +0800

    SPirng In Aciton

--- SringInAction/springinaction/chapter02/src/main/java/chapter04/Audience.java
+++ SringInAction/springinaction/chapter02/src/main/java/chapter04/Audience.java
@@ -1,5 +1,7 @@
 package chapter04;
 
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -10,4 +12,21 @@ public class Audience {
     public void silenceCellPhone() {
         System.out.println("关闭手机");
     }
+    //前置通知
+    @Before("execution(** chapter04.Performance.perform(..))")
+    public void takeSeats() {
+        System.out.println("坐好");
+    }
+    //后置通知
+    @AfterReturning("execution(** chapter04.Performance.perform(..))")
+    public void applause() {
+        System.out.println("鼓掌");
+    }
+    //异常通知
+    @AfterThrowing("execution(** chapter04.Performance.perform(..))")
+    public void demandRefund() {
+        System.out.println("失败了，没关系，重新开始！");
+    }
+
+
 }
