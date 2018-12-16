219c5184652bace87cadf5939c65ea195b15b187
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-12-16 22:23:13 +0800

    第五章

---
+++ SringInAction/springinaction/chapter05/src/main/java/com/chapter05/config/RootConfig.java
@@ -0,0 +1,24 @@
+package com.chapter05.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.type.filter.RegexPatternTypeFilter;
+
+import java.util.regex.Pattern;
+
+@Configuration
+@Import(DataConfig.class)
+@ComponentScan(basePackages={"com.chapter05"},
+    excludeFilters={
+        @Filter(type=FilterType.CUSTOM, value=RootConfig.WebPackage.class)
+    })
+public class RootConfig {
+  public static class WebPackage extends RegexPatternTypeFilter {
+    public WebPackage() {
+      super(Pattern.compile("spittr\\.web"));
+    }    
+  }
+}
--- SringInAction/springinaction/chapter05/src/main/java/com/chapter05/config/SpittrWebAppInitializer.java
+++ SringInAction/springinaction/chapter05/src/main/java/com/chapter05/config/SpittrWebAppInitializer.java
@@ -1,16 +1,19 @@
 package com.chapter05.config;
 
+import com.chapter05.web.WebConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return new Class<?>[] {
+
+        };
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class<?>[] { WebConfig.class };
     }
 
     @Override
