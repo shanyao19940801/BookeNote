9965b83d844c6d4ea29d8d471da3e33b8813fcf4
shanyaoself <sqshanyao@163.com>
2019-02-22 19:47:47 +0800

    springrabbit

---
+++ rabbit/springmq/src/main/java/com/mq/yaomq/YaomqApplication.java
@@ -0,0 +1,13 @@
+package com.mq.yaomq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaomqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YaomqApplication.class, args);
+    }
+
+}
