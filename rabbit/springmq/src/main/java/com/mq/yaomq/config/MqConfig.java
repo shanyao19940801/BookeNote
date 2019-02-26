cf73ca591c7ca8de68da2042b623827f88ba1be5
shanyaoself <sqshanyao@163.com>
2019-02-26 16:07:45 +0800

    add listener

---
+++ rabbit/springmq/src/main/java/com/mq/yaomq/config/MqConfig.java
@@ -0,0 +1,8 @@
+package com.mq.yaomq.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MqConfig {
+
+}
---
+++ rabbit/springmq/src/main/java/com/mq/yaomq/listener/MyListener.java
@@ -0,0 +1,8 @@
+package com.mq.yaomq.listener;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyListener {
+
+}
