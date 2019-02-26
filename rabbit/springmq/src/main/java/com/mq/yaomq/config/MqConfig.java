91ddd2f984b116e0410e4b8f0e23a5a15c0b6aa1
shanyaoself <sqshanyao@163.com>
2019-02-26 18:00:00 +0800

    rabbitmq

--- rabbit/springmq/src/main/java/com/mq/yaomq/config/MqConfig.java
+++ rabbit/springmq/src/main/java/com/mq/yaomq/config/MqConfig.java
@@ -1,8 +1,30 @@
 package com.mq.yaomq.config;
 
+import com.mq.yaomq.listener.MyListener;
+import org.springframework.amqp.rabbit.listener.AbstractMessageListenerContainer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MqConfig {
 
+    @Bean
+    public AbstractMessageListenerContainer commonListenerContainer (
+            @Value("${yao.rabbitmq.queue}") String queue,
+            @Value("${yao.rabbitmq.exchange}") String exchange,
+            MyListener myListener
+//                @Qualifier(CONNECTION_FACTORY) ConnectionFactory connectionFactory
+    ) {
+        return new AbstractMessageListenerContainer() {
+            @Override
+            protected void doInitialize() throws Exception {
+
+            }
+            @Override
+            protected void doShutdown() {
+
+            }
+        };
+    }
 }
--- rabbit/springmq/src/main/java/com/mq/yaomq/listener/MyListener.java
+++ rabbit/springmq/src/main/java/com/mq/yaomq/listener/MyListener.java
@@ -1,8 +1,16 @@
 package com.mq.yaomq.listener;
 
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MyListener {
+public class MyListener implements MessageListener {
 
+    @Override
+    public void onMessage(Message message) {
+        MessageProperties messageProperties = message.getMessageProperties();
+
+    }
 }
