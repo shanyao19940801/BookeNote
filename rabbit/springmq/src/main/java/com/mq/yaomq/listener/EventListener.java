8281b4216aec6fd5f3e623a9a3f6e148ac3cfec0
shanyaoself <sqshanyao@163.com>
2019-03-01 14:50:33 +0800

    mq

--- rabbit/springmq/src/main/java/com/mq/yaomq/listener/EventListener.java
+++ rabbit/springmq/src/main/java/com/mq/yaomq/listener/EventListener.java
@@ -2,12 +2,14 @@ package com.mq.yaomq.listener;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.mq.yaomq.common.event.impl.AsyncEventHandlerManager;
 import com.mq.yaomq.common.rabbit.bean.AsyncEventNotifyRabbitMsgEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
 import org.springframework.amqp.core.MessageProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -18,6 +20,8 @@ import org.springframework.stereotype.Component;
 public class EventListener implements MessageListener {
     private static final Logger logger = LoggerFactory.getLogger(EventListener.class);
     private static final String DEFAULT_CHARSET = "UTF-8";
+    @Autowired
+    AsyncEventHandlerManager asyncEventHandlerManager;
     @Override
     public void onMessage(Message message) {
         MessageProperties messageProperties = message.getMessageProperties();
@@ -37,7 +41,7 @@ public class EventListener implements MessageListener {
         try {
             logger.debug("received {}", JSON.toJSONString(content));
             AsyncEventNotifyRabbitMsgEntity rabbitMessageEntity = JSONObject.parseObject(content, AsyncEventNotifyRabbitMsgEntity.class);
-
+            asyncEventHandlerManager.handleEvent(rabbitMessageEntity.getBody());
         } catch (Exception e){
             logger.error("failed to handle mq message:{}, content:{} ", encoding, content, e);
         }
