37aed8cad8318ef99251b2d90ca320f9c80a2679
shanyaoself <sqshanyao@163.com>
2019-03-06 11:23:33 +0800

    java 动态代理

---
+++ java/javaknowledge/src/main/java/code/dynamic_proxy/JdkProxy.java
@@ -0,0 +1,16 @@
+package code.dynamic_proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * @create by  单耀
+ * @create date  2019/3/6
+ */
+public class JdkProxy implements InvocationHandler {
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return null;
+    }
+}
---
+++ java/javaknowledge/src/main/java/code/dynamic_proxy/User.java
@@ -0,0 +1,26 @@
+package code.dynamic_proxy;
+
+/**
+ * @create by  单耀
+ * @create date  2019/3/6
+ */
+public class User {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
