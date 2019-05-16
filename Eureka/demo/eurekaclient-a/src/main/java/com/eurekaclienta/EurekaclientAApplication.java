50f4d0c7f34124ee33e72c7c0fdcdf2bd9026b3a
shanyaoself <sqshanyao@163.com>
2019-05-16 14:40:41 +0800

    add client

---
+++ Eureka/demo/eurekaclient-a/src/main/java/com/eurekaclienta/EurekaclientAApplication.java
@@ -0,0 +1,13 @@
+package com.eurekaclienta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaclientAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaclientAApplication.class, args);
+    }
+
+}
---
+++ Eureka/demo/eurekaclient-a/src/test/java/com/eurekaclienta/EurekaclientAApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eurekaclienta;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaclientAApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
