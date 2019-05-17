10b997b0952fd89aa9242ac92c68c26ade8fc35c
shanyaoself <sqshanyao@163.com>
2019-05-17 15:15:03 +0800

    高可用注册中心

--- Eureka/demo/eurekaclient-a/src/main/java/com/eurekaclienta/controller/HelloController.java
+++ Eureka/demo/eurekaclient-a/src/main/java/com/eurekaclienta/controller/HelloController.java
@@ -3,19 +3,24 @@ package com.eurekaclienta.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by shanyao on 2019/5/16
  */
+@RestController
 public class HelloController {
 
     private final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
+    @Value("${server.port}")
+    String port;
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
-        logger.info("hello A");
-        return "Hellow A";
+        logger.info("hello A" + " from port:" + port);
+        return "Hellow A" + " from port:" + port;
     }
 }
